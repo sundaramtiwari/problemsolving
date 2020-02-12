b95d605831a7d4f5953303aa46dbde28aae18bfb
Kunal Aggarwal(4145617) <kunalaggarwal4145617@MUMPL-LPM023.local>
2020-02-12 13:09:24 +0100

    added iterative solution for house robber

--- src/main/java/com/dynamicprogramming/houserobber/HouseRobber.java
+++ src/main/java/com/dynamicprogramming/houserobber/HouseRobber.java
@@ -1,5 +1,6 @@
 package com.dynamicprogramming.houserobber;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,20 +17,35 @@ import java.util.List;
  */
 public class HouseRobber {
     public static void main(String[] args) {
-        System.out.println(maxGainOfRobbing(Arrays.asList(4, 15, 3, 9, 5)));
+        System.out.println(maxGainOfRobbingIterative(Arrays.asList(4, 15, 3, 9, 5)));
     }
 
-    private static long maxGainOfRobbing(List<Integer> houseList) {
+    private static int maxGainOfRobbingIterative(List<Integer> houseList) {
+        List<Integer> gainList = new ArrayList<>(houseList.size());
+        gainList.add(0, houseList.get(0));
+        // Gain of second index is the max of robbing first house or the current second house:
+        int gainOfSecondHouse = gainList.get(0) > houseList.get(1) ? gainList.get(0) : houseList.get(1);
+        gainList.add(1, gainOfSecondHouse);
+
+        for (int i = 2; i < houseList.size(); i++) {
+            int gain = Math.max(gainList.get(i - 1), gainList.get(i - 2) + houseList.get(i));
+            gainList.add(i, gain);
+        }
+
+        return gainList.get(gainList.size() - 1);
+    }
+
+    private static long maxGainOfRobbingRecursive(List<Integer> houseList) {
         long maxGain = 0l;
 
         for (int i = 0; i < houseList.size(); i++) {
             int gain = houseList.get(i);
 
             // scan right list for max gain
-            long rightGain = i < houseList.size() - 2 ? maxGainOfRobbing(houseList.subList(i + 2, houseList.size())) : 0;
+            long rightGain = i < houseList.size() - 2 ? maxGainOfRobbingRecursive(houseList.subList(i + 2, houseList.size())) : 0;
 
             // scan left list for max gain
-            long leftGain = i > 1 ? maxGainOfRobbing(houseList.subList(0, i - 1)) : 0;
+            long leftGain = i > 1 ? maxGainOfRobbingRecursive(houseList.subList(0, i - 1)) : 0;
 
             long totalGain = gain + rightGain + leftGain;
             if (maxGain < totalGain) {
